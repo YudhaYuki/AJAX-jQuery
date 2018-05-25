af35a5b84213c29e0c921921343c925dc64439cb
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-25 23:34:05 +0200

    GET data with Ajax

--- js/main.js
+++ js/main.js
@@ -4,9 +4,31 @@ $(document).ready(function() {
     $('.template').load("/templates/template.html");
     */
 
-   $('.template').load("/templates/template.html .innerDiv", function() {
-       console.log('Done loading !')
-   });
+    /*
+    $('.template').load("/templates/template.html .innerDiv", function() {
+        console.log('Done loading !')
+    });
+   */
+
+
+
+
+
+   // ------------------- GET data with Ajax ----------------------
+    var METHOD = "GET";
+    var URL = "https://jsonplaceholder.typicode.com/users";
+
+    $.ajax(URL, {
+        method: METHOD,
+        error: function() {
+            console.log('Something went wrong !');
+        },
+        success: function(data) {
+            console.log(data);
+        }
+    })
+
+
 
 
 });
