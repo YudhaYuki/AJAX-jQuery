64680da9da3f03707e439888ca55f9c798be838c
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-25 23:20:05 +0200

    AJAX - LOAD

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
+    /*
     $('.template').load("/templates/template.html");
+    */
+
+   $('.template').load("/templates/template.html .innerDiv", function() {
+       console.log('Done loading !')
+   });
+
 
 });
 
